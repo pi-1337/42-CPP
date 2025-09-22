7a6481b5b40ba588eeece8ffcdc3319bb7f1041d
Issa Oulkhir <ioulkhir@e2r6p5.1337.ma>
2025-09-22 11:28:54 +0100

    done

--- CPP-01/ex04/main.cpp
+++ CPP-01/ex04/main.cpp
@@ -12,7 +12,7 @@ std::string ft_replace(std::string content, std::string s1, std::string s2)
     {
         content.erase(pos, s1.length());
         content.insert(pos, s2);
-        pos = content.find(s1, 0);
+        pos = content.find(s1, pos + s2.length());
     }
     return content;
 }
@@ -20,27 +20,35 @@ std::string ft_replace(std::string content, std::string s1, std::string s2)
 int main(int ac, char *av[])
 {
     if (ac != 4)
-        return 1;    
+    {
+        std::cerr << "usage:\n\t./io_manipulation <filename> <s1> <s2>" << std::endl;
+        return 1;
+    }
 
     std::string filename = av[1];
     std::string s1 = av[2];
     std::string s2 = av[3];
 
     if (filename.empty() || s1.empty())
+    {
+        std::cerr << "usage:\n\t./io_manipulation <filename> <s1> <s2>" << std::endl;
         return 1;
+    }
     
     // create i/o files
     std::ifstream infile(filename.c_str());
-    std::ofstream outfile((filename + ".replace").c_str());
 
     std::string content;
     if (getline(infile, content, '\0'))
+    {
+        std::ofstream outfile((filename + ".replace").c_str());
         outfile << ft_replace(content, s1, s2);
+        outfile.close();
+    }
     else
         std::cerr << "failed reading file" << std::endl;
     
     infile.close();
-    outfile.close();
 
     return 0;
 }
