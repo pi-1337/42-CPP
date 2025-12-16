92752d03f8aa7db55fc03ba6b928214775f4de09
Issa Oulkhir <ioulkhir@e2r7p5.1337.ma>
2025-12-16 14:14:32 +0100

    iterators are done 4

--- CPP-08/ex02/MutantStack.hpp
+++ CPP-08/ex02/MutantStack.hpp
@@ -1,12 +1,67 @@
-    
+
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
 #include <iostream>
 #include <stack>
 
-class MutantStack: public std::stack {
+template <typename T>
+class MutantStack: public std::stack<T> {
+private:
+
+public:
+    // orthodox
+    MutantStack();
+    MutantStack(const MutantStack& other);
+    MutantStack& operator=(const MutantStack& other);
+    ~MutantStack();
+
+
+    // iterator stuff
+    typedef typename std::stack<T>::container_type Container;
+    typedef typename Container::iterator iterator;
+    
+    iterator begin( void );
+    iterator end( void );
 
 };
+
+
+
+
+// orthodox
+template <typename T>
+MutantStack<T>::MutantStack()
+    : std::stack<T>()
+{
+}
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack& other)
+    : std::stack<T>(other)
+{
+}
+template <typename T>
+MutantStack<T>& MutantStack<T>::operator=(const MutantStack& other)
+{
+   *this = std::stack<T>::operator=(other);
+   return *this;
+}
+template <typename T>
+MutantStack<T>::~MutantStack()
+{
+}
+
+
+// iterator stuff
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin( void )
+{
+    return this->c.begin();
+}
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end( void )
+{
+    return this->c.end();
+}
 
 #endif
--- CPP-08/ex02/main.cpp
+++ CPP-08/ex02/main.cpp
@@ -29,6 +29,45 @@ int main()
         ++it;
     }
     std::stack<int> s(mstack);
+
+
+    std::cout << "\n========== my own TESTS ==========\n" << std::endl;
+
+
+    MutantStack<char> stack_mli7;
+
+    stack_mli7.push(67);
+    stack_mli7.push(80);
+    stack_mli7.push(95);
+    stack_mli7.push(90);
+    stack_mli7.push(51);
+    stack_mli7.push(82);
+    stack_mli7.push(48);
+    stack_mli7.push(123);
+    stack_mli7.push(112);
+    stack_mli7.push(87);
+    stack_mli7.push(52);
+    stack_mli7.push(110);
+    stack_mli7.push(116);
+    stack_mli7.push(48);
+    stack_mli7.push(111);
+    stack_mli7.push(111);
+    stack_mli7.push(79);
+    stack_mli7.push(48);
+    stack_mli7.push(114);
+    stack_mli7.push(125);
+
+    for (
+        MutantStack<char>::iterator it = stack_mli7.begin();
+        it != stack_mli7.end();
+        it++
+    )
+    {
+        std::cout << static_cast<char>(*it);
+    }
+
+    std::cout << "\n\n========== end TESTS ==========\n" << std::endl;
+
     return 0;
 }
 
